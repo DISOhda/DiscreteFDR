baf813fe0b96f64820bb97740ce4afcc8d42cdf8
DISOhda <diso.fbmn@h-da.de>
2024-10-16 15:40:30 +0200

    Documentation corrections

--- man-roxygen/return.R
+++ man-roxygen/return.R
@@ -5,13 +5,13 @@
 #' \item{Num.rejected}{number of rejections.}
 #' \item{Adjusted}{adjusted p-values<%=ifelse(exists("BR") && BR, ""," (only for step-down direction)")%>.}
 #' \item{Critical.constants}{critical values (only exists if computations where performed with `ret.crit.consts = TRUE`).}
-#' \item{Select}{list with data related to \eqn{p}-value selection; only exists if `threshold < 1`.}
-#' \item{Select$Threshold}{p-value selection `threshold`.}
+#' \item{Select}{list with data related to \eqn{p}-value selection; only exists if `select.threshold < 1`.}
+#' \item{Select$Threshold}{p-value selection threshold (`select.threshold`).}
 #' \item{Select$Effective.Thresholds}{results of each p-value CDF evaluated at the selection threshold.}
-#' \item{Select$Pvalues}{selected p-values that are \eqn{\leq} selection `threshold`.}
-#' \item{Select$Indices}{indices of p-values \eqn{\leq} selection `threshold`.}
+#' \item{Select$Pvalues}{selected p-values that are \eqn{\leq} selection threshold.}
+#' \item{Select$Indices}{indices of p-values \eqn{\leq} selection threshold.}
 #' \item{Select$Scaled}{scaled selected p-values.}
-#' \item{Select$Number}{number of selected p-values \eqn{\leq} `threshold`.}
+#' \item{Select$Number}{number of selected p-values \eqn{\leq} selection threshold.}
 #' \item{Data}{list with input data.}
 #' \item{Data$Method}{character string describing the used algorithm, e.g. 'Discrete Benjamini-Hochberg procedure (step-up)'}
 #' \item{Data$Raw.pvalues}{observed p-values.}
