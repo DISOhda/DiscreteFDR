249fb6bebf2a27c3fe16053980e7b2d06fce449c
DISOhda <diso.fbmn@h-da.de>
2024-10-16 13:35:41 +0200

    Make function internal; tweaks to formatting

--- R/Aux_fun.R
+++ R/Aux_fun.R
@@ -1,5 +1,7 @@
 #' @name match.pvals
 #' 
+#' @keywords internal
+#' 
 #' @title
 #' Matching Raw P-Values with Supports 
 #' 
@@ -11,31 +13,30 @@
 #' respective CDFs (as in function `p.discrete.adjust()` of package
 #' `discreteMTP`, which is no longer available on CRAN).
 #' 
-#' **Note**: In the next version, this is to become an internal function and
-#' will have to be called directly via `:::`, i.e.
-#' `DiscreteFDR:::match.pvals()`.
+#' **Note**: This is an internal function and has to be called directly via
+#' `:::`, i.e. `DiscreteFDR:::match.pvals()`.
 #' 
 #' @details
 #' Well computed raw p-values should already belong to their respective CDF
-#' support. So this function is called at the beginning of [discrete.BH()],
-#' [DBH()], [ADBH()] and [DBR()], just in case raw p-values are biased.
+#' support. So this function is called at the beginning of [`discrete.BH()`],
+#' [`DBH()`], [`ADBH()`] and [`DBR()`], just in case raw p-values are biased.
 #'
 #' For each raw p-value that needs to be rounded, a warning is issued.
 #'
 #' @seealso
 #' [`discrete.BH()`], [`DBR()`]
 #'
+#' @templateVar test.results TRUE
 #' @templateVar pCDFlist TRUE
-#' @templateVar raw.pvalues TRUE
 #' @templateVar pCDFlist.indices TRUE
 #' @template param
 #' 
 #' @examples \dontrun{
 #' toyList <- list(c(0.3,0.7,1),c(0.1,0.65,1))
 #' toyRaw1 <- c(0.3,0.65)
-#' match.pvals(toyList,toyRaw1)
+#' match.pvals(toyRaw1, toyList)
 #' toyRaw2 <- c(0.31,0.6)
-#' match.pvals(toyList,toyRaw2)
+#' match.pvals(toyRaw2, toyList)
 #' }
 #'
 #' @return
@@ -44,40 +45,35 @@
 #'
 #' @importFrom lifecycle deprecate_soft
 #' @export
-match.pvals <- function(pCDFlist, raw.pvalues, pCDFlist.indices = NULL){
-  deprecate_soft("2.0.0", "match.pvals()", 
-                 details = paste("This will become a purely internal",
-                                 "function. Please call it directly via",
-                                 "'DiscreteFDR:::match.pvals()'.")
-                 )
-  
-  m <- length(raw.pvalues)
-  if(!is.null(pCDFlist.indices)){
+match.pvals <- function(test.results, pCDFlist, pCDFlist.indices = NULL) {
+  m <- length(test.results)
+  if(!is.null(pCDFlist.indices)) {
     idx <- unlist(pCDFlist.indices)
     counts <- sapply(pCDFlist.indices, length)
     pCDFlist <- rep(pCDFlist, counts)[order(idx)]
   }
   n <- length(pCDFlist)
-  if(m > 0 && m == n){
-    pvec <- raw.pvalues
+  if(m > 0 && m == n) {
+    pvec <- test.results
     in.CDF <- numeric(m)
-    for (k in 1:m) {
+    for(k in seql_len(m)) {
       in.CDF[k] <- match(pvec[k], pCDFlist[[k]])
-      if (is.na(in.CDF[k])){
+      if(is.na(in.CDF[k])) {
         in.CDF[k] <- which.min(abs(pCDFlist[[k]] - pvec[k]))
         pvec[k] <- pCDFlist[[k]][in.CDF[k]]
         ordinal <- "th"
-        if (k%%10==1) ordinal <- "st"
-        if (k%%10==2) ordinal <- "nd"
-        if (k%%10==3) ordinal <- "rd"
-        if (k%%100-k%%10==10) ordinal <- "th"
-        warning("Since ", raw.pvalues[k], 
-                " is not a value of the CDF of the ",k,ordinal ," p-value,\n  the p-value is rounded to be ", 
-                pCDFlist[[k]][in.CDF[k]], call. = F)
+        if(k %% 10 == 1) ordinal <- "st"
+        if(k %% 10 == 2) ordinal <- "nd"
+        if(k %% 10 == 3) ordinal <- "rd"
+        if(k %% 100 - k %% 10 == 10) ordinal <- "th"
+        warning("Since ", test.results[k], 
+                " is not a value of the CDF of the ", k, ordinal, " p-value,\n",
+                "  the p-value is rounded to be ", pCDFlist[[k]][in.CDF[k]],
+                call. = F)
       }
     }
     return(pvec)
-  }else{
-    stop("'pCDFlist' and 'raw.pvalues' must have the same non-zero length")
+  } else {
+    stop("'pCDFlist' and 'test.results' do not match")
   }
 }
