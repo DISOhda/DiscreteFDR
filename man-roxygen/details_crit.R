3c7ed4379941842f68b1919e0bd5d48289791952
DISOhda <diso.fbmn@h-da.de>
2024-12-09 10:47:31 +0100

    Minor documentation update

--- man-roxygen/details_crit.R
+++ man-roxygen/details_crit.R
@@ -2,5 +2,5 @@
 #' Computing critical constants (`ret.crit.consts = TRUE`) requires considerably
 #' more execution time, especially if the number of unique supports is large.
 #' We recommend that users should only have them calculated when they need them,
-#' e.g. for illustrating the rejection area in a plot or other theoretical
+#' e.g. for illustrating the rejection set in a plot or other theoretical
 #' reasons.
