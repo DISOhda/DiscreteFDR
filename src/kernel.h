844a41c863fa401bfae150291b9d0d0388237b11
DISOhda <diso.fbmn@h-da.de>
2024-12-14 11:18:01 +0100

    Reformatting and removal of examples (CRAN no longer allows examples for non-exported functions)

--- src/kernel.h
+++ src/kernel.h
@@ -65,50 +65,6 @@
 //' 
 //' @seealso
 //' [`discrete.BH()`], [`direct.discrete.BH()`], [`DBR()`]
-//' 
-//' @examples \dontrun{
-//' X1 <- c(4, 2, 2, 14, 6, 9, 4, 0, 1)
-//' X2 <- c(0, 0, 1, 3, 2, 1, 2, 2, 2)
-//' N1 <- rep(148, 9)
-//' N2 <- rep(132, 9)
-//' Y1 <- N1 - X1
-//' Y2 <- N2 - X2
-//' df <- data.frame(X1, Y1, X2, Y2)
-//' df
-//' 
-//' # Compute p-values and their supports of Fisher's exact test
-//' test.result <- generate.pvalues(df, "fisher")
-//' raw.pvalues <- test.result$get_pvalues()
-//' pCDFlist <- test.result$get_pvalue_supports()
-//' 
-//' alpha <- 0.05
-//' 
-//' # Compute the step functions from the supports
-//' 
-//' # If not searching for critical constants, we use only the observed p-values
-//' sorted.pvals   <- sort(raw.pvalues)
-//' y.DBH.sd.fast  <- DiscreteFDR:::kernel_DBH_fast(pCDFlist, sorted.pvals)
-//' y.ADBH.sd.fast <- DiscreteFDR:::kernel_ADBH_fast(pCDFlist, sorted.pvals)
-//' y.DBR.fast     <- DiscreteFDR:::kernel_DBR_fast(pCDFlist, sorted.pvals)
-//' # transformed values
-//' y.DBH.sd.fast
-//' y.ADBH.sd.fast
-//' y.DBR.fast
-//' 
-//' # compute transformed support
-//' pv.list        <- sort(unique(unlist(pCDFlist)))
-//' y.DBH.sd.crit  <- DiscreteFDR:::kernel_DBH_crit(pCDFlist, pv.list, sorted.pvals)
-//' y.ADBH.sd.crit <- DiscreteFDR:::kernel_ADBH_crit(pCDFlist, pv.list, sorted.pvals)
-//' y.DBR.crit     <- DiscreteFDR:::kernel_DBR_crit(pCDFlist, pv.list, sorted.pvals)
-//' # critical constants
-//' y.DBH.sd.crit$crit.consts
-//' y.ADBH.sd.crit$crit.consts
-//' y.DBR.crit$crit.consts
-//' # The following exist only for step-down direction or DBR
-//' y.DBH.sd.crit$pval.transf
-//' y.ADBH.sd.crit$pval.transf
-//' y.DBR.crit$pval.transf
-//' }
 //'
 struct tau_m_results {
   double value;
@@ -116,46 +72,95 @@ struct tau_m_results {
   std::vector<double> eval;
 };
 
-tau_m_results DBH_tau_m(const NumericVector* sfuns, const NumericVector& CDFcounts, const int numCDF, const int* lens, const NumericVector& support, const int numTests, const double alpha);
+tau_m_results DBH_tau_m(
+  const NumericVector* sfuns,
+  const NumericVector& CDFcounts,
+  const int numCDF,
+  const int* lens,
+  const NumericVector& support,
+  const int numTests,
+  const double alpha
+);
 
-//tau_m_results DBH_tau_m2(const NumericVector *sfuns, const NumericVector &CDFcounts, const int numCDF, const NumericVector &support, const int numTests, const double alpha, bool adaptive = false);
-
-///' @export
 //' @rdname kernel
 // [[Rcpp::export]]
-NumericVector kernel_DBH_fast(const List &pCDFlist, const NumericVector &pvalues, const bool stepUp = false, const Nullable<NumericVector> tau_max = R_NilValue, const double alpha = 0.05, const NumericVector &support = NumericVector(), const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+NumericVector kernel_DBH_fast(
+  const List& pCDFlist,
+  const NumericVector& pvalues,
+  const bool stepUp = false,
+  const Nullable<NumericVector> tau_max = R_NilValue,
+  const double alpha = 0.05,
+  const NumericVector& support = NumericVector(),
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
 
-///' @export
 //' @rdname kernel
 // [[Rcpp::export]]
-List kernel_DBH_crit(const List &pCDFlist, const NumericVector &support, const NumericVector &sorted_pv, const bool stepUp = false, const double alpha = 0.05, const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+List kernel_DBH_crit(
+  const List& pCDFlist,
+  const NumericVector& support,
+  const NumericVector& sorted_pv,
+  const bool stepUp = false,
+  const double alpha = 0.05,
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
 
-///' @export
 //' @rdname kernel
 // [[Rcpp::export]]
-NumericVector kernel_ADBH_fast(const List &pCDFlist, const NumericVector &sorted_pv, const bool stepUp = false, const double alpha = 0.05, const NumericVector &support = NumericVector(), const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+NumericVector kernel_ADBH_fast(
+  const List& pCDFlist,
+  const NumericVector& sorted_pv,
+  const bool stepUp = false,
+  const double alpha = 0.05,
+  const NumericVector& support = NumericVector(),
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
 
-/// '@export
 //' @rdname kernel
 // [[Rcpp::export]]
-List kernel_ADBH_crit(const List &pCDFlist, const NumericVector &support, const NumericVector &sorted_pv, const bool stepUp = false, const double alpha = 0.05, const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+List kernel_ADBH_crit(
+  const List& pCDFlist,
+  const NumericVector& support,
+  const NumericVector& sorted_pv,
+  const bool stepUp = false,
+  const double alpha = 0.05,
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
 
-///' @export
 //' @rdname kernel
 // [[Rcpp::export]]
-NumericVector kernel_DBR_fast(const List &pCDFlist, const NumericVector &sorted_pv, const double lambda = 0.05, const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+NumericVector kernel_DBR_fast(
+  const List& pCDFlist,
+  const NumericVector& sorted_pv,
+  const double lambda = 0.05,
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
 
-///' @export
 //' @rdname kernel
 // [[Rcpp::export]]
-List kernel_DBR_crit(const List &pCDFlist, const NumericVector &support, const NumericVector &sorted_pv, const double lambda = 0.05, const double alpha = 0.05, const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+List kernel_DBR_crit(
+  const List& pCDFlist,
+  const NumericVector& support,
+  const NumericVector& sorted_pv,
+  const double lambda = 0.05,
+  const double alpha = 0.05,
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
 
-///' @export
 //' @rdname kernel
 // [[Rcpp::export]]
-NumericVector kernel_DBY_fast(const List &pCDFlist, const NumericVector &pvalues, const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+NumericVector kernel_DBY_fast(
+  const List& pCDFlist,
+  const NumericVector& pvalues,
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
 
-///' @export
 //' @rdname kernel
 // [[Rcpp::export]]
-List kernel_DBY_crit(const List &pCDFlist, const NumericVector &support, const NumericVector &sorted_pv, const double alpha = 0.05, const Nullable<NumericVector> &pCDFcounts = R_NilValue);
+List kernel_DBY_crit(
+  const List& pCDFlist,
+  const NumericVector& support,
+  const NumericVector& sorted_pv,
+  const double alpha = 0.05,
+  const Nullable<NumericVector>& pCDFcounts = R_NilValue
+);
