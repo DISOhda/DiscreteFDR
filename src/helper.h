e327f0434a93cfb609dd37f9741c67afc08f05db
DISOhda <diso.fbmn@h-da.de>
2024-12-14 11:14:52 +0100

    Reformatting and removal of old commented out code

--- src/helper.h
+++ src/helper.h
@@ -1,28 +1,13 @@
-// //[[Rcpp::depends(RcppClock)]]
 #include <RcppArmadillo.h>
-//#include <RcppClock.h>
 using namespace Rcpp;
 
-// fast step function evaluation
-// step function is represented by a single numeric vector under the conditions
-// a) f(x) = x and b) x is sorted
-// this is much faster than passing and evaluating R step function objects
-// struct stepfun_result{
-//   NumericVector y;
-//   int last_index;
-// };
-// 
-// stepfun_result stepfun(const NumericVector &x, const NumericVector &sfun, const int &start = 0);
-
-// NumericVector stepfun(const NumericVector &x, const NumericVector &sfun);
-
-// int stepfun_idx(const double &x, const NumericVector &sfun, const int &start)
-
-// shortcut function that eliminates all values of a SORTED vector that
-// are < limit, except the largest value <= limit
-// NumericVector short_eff(const NumericVector &x, const double limit);
-
-inline void eval_pv(double &eval, double val, const NumericVector &vec, int len, int &pos){
+inline void eval_pv(
+  double& eval,
+  double val,
+  const NumericVector& vec,
+  int len,
+  int& pos
+) {
   //if(val < 1){
     while(pos < len && vec[pos] <= 1 && vec[pos] <= val) pos++;
     if(pos) eval = vec[pos - 1];
@@ -30,7 +15,12 @@ inline void eval_pv(double &eval, double val, const NumericVector &vec, int len,
   //} else eval = 1;
 }
 
-inline void eval_pv_rev(double &eval, double val, const NumericVector &vec, int &pos){
+inline void eval_pv_rev(
+  double& eval,
+  double val,
+  const NumericVector& vec,
+  int& pos
+) {
   //if(val < 1){
     while(pos > 0 && (vec[pos] > val || vec[pos] > 1)) pos--;
     if(vec[pos] <= val) eval = vec[pos];
@@ -39,7 +29,11 @@ inline void eval_pv_rev(double &eval, double val, const NumericVector &vec, int 
 }
 
 // computes the index of the largest element of a vector which is <= a given value
-inline int binary_search(const NumericVector &vec, const double value, const int len) {
+inline int binary_search(
+  const NumericVector& vec,
+  const double value,
+  const int len
+) {
   int idx_left = 0, idx_right = len - 1, idx_mid = len - 1;
   bool stop = false;
   
@@ -55,7 +49,10 @@ inline int binary_search(const NumericVector &vec, const double value, const int
         idx_mid = idx_left + (idx_right - idx_left) / 2;
       }
     } else if(vec[idx_mid] <= value) {
-      if(vec[idx_mid] == value || idx_mid == len - 1 || idx_right - idx_mid == 1) {
+      if(vec[idx_mid] == value ||
+         idx_mid == len - 1 ||
+         idx_right - idx_mid == 1
+      ) {
         stop = true;
       } else {
         idx_left = idx_mid;
@@ -68,12 +65,7 @@ inline int binary_search(const NumericVector &vec, const double value, const int
 }
 
 // function that binds two vectors, sorts it and eliminates duplications 
-NumericVector sort_combine(const NumericVector &x, const NumericVector &y);
-
-// sort columns of a matrix in descending order
-// using an intermediate numeric vector is necessary, because "in-column"
-// sorting does not always work as expected, especially for large columns
-//void colsortdec(NumericMatrix &mat);
+NumericVector sort_combine(const NumericVector& x, const NumericVector& y);
 
 // sort order
-IntegerVector order(const NumericVector &x, bool descending = false);
+IntegerVector order(const NumericVector& x, bool descending = false);
--- src/kernel_common.cpp
+++ src/kernel_common.cpp
@@ -1,6 +1,14 @@
 #include "kernel.h"
 
-tau_m_results DBH_tau_m(const NumericVector* sfuns, const NumericVector& CDFcounts, const int numCDF, const int* lens, const NumericVector& support, const int numTests, const double alpha) {
+tau_m_results DBH_tau_m(
+  const NumericVector* sfuns,
+  const NumericVector& CDFcounts,
+  const int numCDF,
+  const int* lens,
+  const NumericVector& support,
+  const int numTests,
+  const double alpha
+) {
   // size of the p-value support
   int numValues = support.length();
   
@@ -16,9 +24,13 @@ tau_m_results DBH_tau_m(const NumericVector* sfuns, const NumericVector& CDFcoun
   for(int i = 0; i < numCDF; i++) pos[i] = lens[i] - 1;
   
   // first valid p-value in support
-  int idx_first_valid = binary_search(support, alpha / (1 + alpha), numValues) + 1;
+  int idx_first_valid = 1 + binary_search(
+    support, alpha / (1 + alpha), numValues
+  );
   // positions for binary search
-  int idx_left = idx_first_valid, idx_right = numValues - 1, idx_mid = numValues - 1;
+  int idx_left = idx_first_valid,
+      idx_right = numValues - 1,
+      idx_mid = numValues - 1;
   // bool variable to indicate end of search
   bool stop = false;
   // traverse step functions increasingly (start with "false")
@@ -70,7 +82,10 @@ tau_m_results DBH_tau_m(const NumericVector* sfuns, const NumericVector& CDFcoun
       }
     } else {
       // tau_m COULD be larger than the current p-value
-      if(idx_mid == numValues - 1 || sum == numTests * alpha || idx_right - idx_mid == 1) {
+      if(idx_mid == numValues - 1 ||
+         sum == numTests * alpha ||
+         idx_right - idx_mid == 1
+      ) {
         // if difference between mid and right position equals 1 or the largest
         //   p-value satisfies the condition or sum equals threshold, then we
         //   found tau_m
@@ -90,40 +105,3 @@ tau_m_results DBH_tau_m(const NumericVector* sfuns, const NumericVector& CDFcoun
   
   return {tau_m, idx_tau, tau_m_eval};
 }
-
-/*tau_m_results DBH_tau_m2(const NumericVector *sfuns, const NumericVector &CDFcounts, const int numCDF, const NumericVector &support, const int numTests, const double alpha, bool adaptive) {
-  // size of the p-value support
-  int len = support.length();
-  
-  // search for tau_m
-  int* pos = new int[numCDF];
-  for(int j = 0; j < numCDF; j++) pos[j] = sfuns[j].length() - 1;
-  double tau_m = 1, sum = numTests * alpha + 1, eval = 0;
-  int idx_tau = len;
-  std::vector<double> tau_m_eval(numCDF);
-  while(idx_tau > 0 && sum > numTests * alpha){
-    idx_tau--;
-    sum = 0;
-    int j = 0;
-    while(j < numCDF && sum <= numTests * alpha){
-      eval_pv_rev(eval, support[idx_tau], sfuns[j], pos[j]);
-      //if(adaptive)
-      //  sum = std::max<double>(sum, CDFcounts[j] * (eval / (1 - eval)));
-      //else
-      sum += CDFcounts[j] * (eval / (1 - eval));
-      j++;
-    }
-  }
-  // if tau_m is found, compute evaluated F_i
-  if(sum <= numTests * alpha){
-    tau_m = support[idx_tau];
-    for(int j = 0; j < numCDF; j++){
-      eval_pv_rev(tau_m_eval[j], tau_m, sfuns[j], pos[j]);
-    }
-  }
-  
-  // garbage collection
-  delete[] pos;
-  
-  return {tau_m, idx_tau, tau_m_eval};
-}*/
